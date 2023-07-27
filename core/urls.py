7bdb347ae744fa2a34f85f0e6b59269f29757ba5
booorayan <boorayo@yahoo.com>
2023-07-27 16:18:35 +0300

    login

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
+from django.contrib.auth import views
 from django.urls import path
 
 from .views import *
+from .forms import LoginForm
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
-    path('signup/', signup, name='signup')
+    path('signup/', signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),
 ]
