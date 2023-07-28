d1571b349704249fa8322afb2aecd6bacb15e152
booorayan <boorayo@yahoo.com>
2023-07-28 12:22:06 +0300

    add items

--- items/urls.py
+++ items/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .views import *
 
+app_name = 'items'
+
 urlpatterns = [
+    path('new/', new, name='new'),
     path('<int:pk>/', detail, name='detail'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+
 from .models import *
+from .forms import NewItemForm
 
 
 # Create your views here.
@@ -13,3 +16,23 @@ def detail(request, pk):
     }
 
     return render(request, 'detail.html', context)
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'New Item',
+    })
