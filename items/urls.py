dda709cc395f97e09617d98ba7692cdd69174801
booorayan <boorayo@yahoo.com>
2023-07-28 16:10:09 +0300

    update items

--- items/urls.py
+++ items/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('new/', new, name='new'),
     path('<int:pk>/', detail, name='detail'),
     path('<int:pk>/delete/', delete, name='delete'),
+    path('<int:pk>/edit/', edit, name='edit'),
 ]
--- items/views.py
+++ items/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import *
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 
 # Create your views here.
@@ -35,6 +35,26 @@ def new(request):
     return render(request, 'form.html', {
         'form': form,
         'title': 'New Item',
+    })
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'form.html', {
+        'form': form,
+        'title': 'Edit Item',
     })
 
 
